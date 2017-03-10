9e610c13db3e14eb5072c9a87398f4dbfef01d0c
BrandonYates <brandonyates66@gmail.com>
2017-03-09 20:56:03 -0600

    definied the Utilities interface and an implmentation

---
+++ SharedProject1/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YatesUtils
+{
+    interface Utilities
+    {
+        public String getDate(long epoch);
+
+        public long fromDateString(String dateString);
+
+        public String helloWorld();
+    };
+}
---
+++ SharedProject1/UtilitiesImpl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YatesUtils
+{
+    class UtilitiesImpl : Utilities
+    {
+        //takes milliseconds since the EPOCH and returns a string representation
+        public String getDate(long unixTime)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            DateTime actual = epoch.AddMilliseconds(unixTime);
+
+            return actual.ToShortDateString();
+        }
+
+        //takes in a string and returns the milliseconds since the epoch in UTC
+        public long fromDateString(String dateString)
+        {
+            DateTime dateTime = Convert.ToDateTime(dateString);
+
+            return dateTime.ToUniversalTime().Subtract(
+                new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+        }
+
+        //classic hello world method, mostly used for testing if the service is responding
+        public String helloWorld()
+        {
+            return "Hello World\n";
+        }
+    }
+}
